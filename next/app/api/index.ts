5c16dce1a3cd2792f68bc840edc5ea9ca2c7e178
GITZMBE <lucasfilip@hotmail.se>
2024-04-02 15:09:27 +0200

    Add general graphql request function

---
+++ next/app/api/index.ts
@@ -0,0 +1,18 @@
+const graphqlRequest = async (query: string) => {
+  const url = process.env.NEXT_PUBLIC_BASEURL || "";
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Conent-Type": "application/json"
+    },
+    body: JSON.stringify({
+      query
+    })
+  });
+
+  const data = await res.json();
+  return data;
+};
+
+export default graphqlRequest;
