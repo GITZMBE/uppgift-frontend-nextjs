08c8ff76b7188a2022358032eebb7f05bf173601
GITZMBE <lucasfilip@hotmail.se>
2024-04-02 19:26:22 +0200

    Create GraphQL request function

---
+++ next/app/lib/datocms.ts
@@ -0,0 +1,23 @@
+import { GraphQLClient } from "graphql-request";
+
+interface RequestOptions {
+  query: string;
+  variable?: Record<string, any>;
+  preview: boolean;
+};
+
+const request: React.FC<RequestOptions> = ({ query, variable, preview }) => {
+  const endpoint: string = preview
+    ? "https://graphql.datocms.com/preview"
+    : "https://graphql.datocms.com";
+
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN}`,
+    },
+  });
+
+  return client.request(query, variable);
+};
+
+export default request;
