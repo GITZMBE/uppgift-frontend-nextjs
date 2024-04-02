489b9c0d77de0cc53bf41d0f87375d0c794ed157
GITZMBE <lucasfilip@hotmail.se>
2024-04-02 21:00:23 +0200

    Add data to product- and home page

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,9 +1,98 @@
+import { StructuredText } from "react-datocms";
 import Container from "./components/Container";
+import request from "./lib/datocms";
 
-export default function Home() {
+// export const getStaticProps = async () => {
+//   const query = `query productCopy {
+//     allProducts {
+//       price
+//       name
+//       mainImage {
+//         url
+//         width
+//         title
+//         size
+//         id
+//         height
+//         format
+//         filename
+//         author
+//         alt
+//         responsiveImage {
+//           width
+//           title
+//           srcSet
+//           src
+//           sizes
+//           height
+//           webpSrcSet
+//           bgColor
+//           base64
+//           aspectRatio
+//           alt
+//         }
+//       }
+//       id
+//     }
+//   }`;
+
+//   const data = await request({ query });
+
+//   return {
+//     props: { 
+//       data 
+//     }
+//   };
+// };
+
+const Home = async () => {
+  const query = `query productCopy {
+    allProducts {
+      price
+      name
+      mainImage {
+        url
+        width
+        title
+        size
+        id
+        height
+        format
+        filename
+        author
+        alt
+        responsiveImage {
+          width
+          title
+          srcSet
+          src
+          sizes
+          height
+          webpSrcSet
+          bgColor
+          base64
+          aspectRatio
+          alt
+        }
+      }
+      id
+      description {
+        value
+      }
+    }
+  }`;
+  const data = await request({ query });
+  const products = data?.allProducts || [];
+
   return (
     <Container>
-      Home
+      {
+        products && products.map(product => {
+          <StructuredText data={product.description.value} />
+        })
+      }
     </Container>
   );
-}
+};
+
+export default Home;
