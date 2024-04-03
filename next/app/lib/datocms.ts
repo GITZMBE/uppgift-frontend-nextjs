0ab5be11dc6ff90b3c2ccefb272312e9dc5f1b3b
GITZMBE <lucasfilip@hotmail.se>
2024-04-03 09:28:38 +0200

    Change datocms Remove preview prop

--- next/app/lib/datocms.ts
+++ next/app/lib/datocms.ts
@@ -3,13 +3,10 @@ import { GraphQLClient } from "graphql-request";
 interface RequestOptions {
   query: string;
   variable?: Record<string, any>;
-  preview?: boolean;
 };
 
-const request: React.FC<RequestOptions> = ({ query, variable, preview }) => {
-  const endpoint: string = preview
-    ? "https://graphql.datocms.com/preview"
-    : "https://graphql.datocms.com";
+const request: React.FC<RequestOptions> = ({ query, variable }) => {
+  const endpoint: string = process.env.NEXT_PUBLIC_DATOCMS_API_BASEURL || "";
 
   const client = new GraphQLClient(endpoint, {
     headers: {
