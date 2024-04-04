9cc09648a57ff30616b6445bdadbc17fd6ad59db
GITZMBE <lucasfilip@hotmail.se>
2024-04-04 14:52:27 +0200

    Create recoil state cartState

--- next/app/recoil/index.ts
+++ next/app/recoil/index.ts
@@ -1,6 +1,16 @@
 import { atom } from "recoil";
+import CartItem from "../models/cartItem";
 
-export const cartState = atom({
+interface ICartState {
+  key: string;
+  default: CartItem[];
+}
+
+const defaultCartState: ICartState = {
   key: "cartState",
   default: []
+};
+
+export const cartState = atom<ICartState>({
+  ...defaultCartState
 });
