c8a063de0e9c3b6cdba6a57e49890437ee4c8e83
GITZMBE <lucasfilip@hotmail.se>
2024-04-05 11:28:26 +0200

    Rewrite how to adding product to cart in the product page

--- next/app/components/CartItemRecord.tsx
+++ next/app/components/CartItemRecord.tsx
@@ -24,7 +24,7 @@ const CartItemRecord = ({ item }: { item: CartItem }) => {
           <span className='w-8 text-center text-xl'>{ quantity }</span>
           <button className='flex justify-center items-center w-12 aspect-square rounded-full text-3xl' onClick={_ => setQuantity(quantity + 1)}><FaPlusCircle size={36} className="opacity-75 hover:opacity-100" /></button>
         </div>
-        <span className='w-20 text-center text-2xl'>$ { item.price * quantity }</span>
+        <span className='w-28 text-center text-2xl'>$ { item.price * quantity }</span>
         <RxCross2 size={24} className='absolute right-4 hidden group-hover:block opacity-50 hover:opacity-100 cursor-pointer' onClick={_ => setQuantity(0)} />
       </div>
     </div>
