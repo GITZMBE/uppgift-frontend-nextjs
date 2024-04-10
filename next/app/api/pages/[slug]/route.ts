edac4036aa0ddcbf81f137aa86e74aaf89568e30
GITZMBE <lucasfilip@hotmail.se>
2024-04-10 08:13:18 +0200

    Update route /api/pages/[id]

---
+++ next/app/api/pages/[slug]/route.ts
@@ -0,0 +1,27 @@
+import request from "@/src/lib/datocms";
+import Page from "@/src/models/page";
+
+export async function GET(req: Request, context: any) {
+  const { params } = context;
+  const { slug } = params;
+  
+  const query = `query productCopy {
+    page(filter: {slug: {eq: "${slug}"}}) {
+      id
+      title
+      mainImage {
+        responsiveImage(imgixParams: {auto: format, w: "2000", fit: crop, h: "2000"}) {
+          srcSet
+          src
+          width
+          height
+        }
+      }
+      content {
+        value
+      }
+    }
+  }`;
+  const { page } = await request<{ page: Page }>({ query });
+  return new Response(JSON.stringify({ page: page }));
+}
